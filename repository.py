5a1eb24f2a007c06c68185007ea6c9452720ede7
Magdaena Rother <rother.magdalena@gmail.com>
2022-02-20 11:53:26 +0100

    Add repository

---
+++ repository.py
@@ -0,0 +1,27 @@
+from abc import ABC, abstractmethod
+
+import model
+
+
+class AbstractRepository(ABC):
+    @abstractmethod
+    def add(self, batch: model.Batch):
+        raise NotImplementedError
+
+    @abstractmethod
+    def get(self, ref) -> model.Batch:
+        raise NotImplementedError
+
+
+class SqlAlchemyRepository(AbstractRepository):
+    def __init__(self, session):
+        self.session = session
+
+    def add(self, batch):
+        self.session.add(batch)
+
+    def get(self, ref):
+        return self.session.query(model.Batch).filter_by(ref=ref).one()  # noqa
+
+    def list(self):
+        return self.session.query(model.Batch).all()
